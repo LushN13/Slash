e583ad19e325a896476209ecb446cbff31c31e52
LushN13 <1292735722@qq.com>
2026-03-04 23:18:22 +0800

    优化了第三人称视角的摄像机运动，将人物移动转向与摄像机运动分离

--- Source/Slash/Private/Player/Characters/SlashCharacter.cpp
+++ Source/Slash/Private/Player/Characters/SlashCharacter.cpp
@@ -2,4 +2,43 @@
 
 
 #include "Player/Characters/SlashCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+
+// 在ASlashCharacter构造函数中添加CameraBoom的创建和初始化
+ASlashCharacter::ASlashCharacter()
+{
+    PrimaryActorTick.bCanEverTick = true;
+    
+    //禁止角色跟随控制器旋转
+    bUseControllerRotationPitch = false;
+    bUseControllerRotationYaw = false; 
+    bUseControllerRotationRoll = false;
+ 
+    //创建弹簧臂 CameraBoom
+    CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+    CameraBoom->SetupAttachment(GetRootComponent()); // 绑在根组件上
+    CameraBoom->TargetArmLength = 300.f; // 默认臂长
+   
+    //弹簧臂使用控制器的旋转
+    CameraBoom->bUsePawnControlRotation = true;
+
+    //配置角色移动组件,让角色朝向移动方向
+    GetCharacterMovement()->bOrientRotationToMovement = true;
+    GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+   
+    //创建摄像机 ViewCamera
+    ViewCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("ViewCamera"));
+    ViewCamera->SetupAttachment(CameraBoom); //绑在弹簧臂末端
+    
+    //摄像机不随控制器旋转，跟着弹簧臂
+    ViewCamera->bUsePawnControlRotation = false;
+   
+  
+    
+ 
+
+}
 
--- Source/Slash/Public/Player/Characters/SlashCharacter.h
+++ Source/Slash/Public/Player/Characters/SlashCharacter.h
@@ -6,12 +6,30 @@
 #include "Player/Characters/SlashCharacterBase.h"
 #include "SlashCharacter.generated.h"
 
-/**
- * 
- */
+class USpringArmComponent;
+class UCameraComponent;
+
+
+
 UCLASS()
 class SLASH_API ASlashCharacter : public ASlashCharacterBase
 {
 	GENERATED_BODY()
+
+public:
+	ASlashCharacter();
+	//提供给其他类访问的接口
+	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	FORCEINLINE UCameraComponent* GetViewCamera() const { return ViewCamera; }
+protected:
+
+
+private:
+	// 弹簧臂组件
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USpringArmComponent> CameraBoom;
+	//摄像机组件
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UCameraComponent> ViewCamera;
 	
 };
