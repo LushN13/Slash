63972a9b23eab53824bfe515a7e07423685c86b7
LushN13 <1292735722@qq.com>
2026-03-04 22:36:57 +0800

    添加人物骨骼插槽，修复并完成玩家角色的移动控制

--- Source/Slash/Private/Player/Characters/SlashCharacterBase.cpp
+++ Source/Slash/Private/Player/Characters/SlashCharacterBase.cpp
@@ -9,6 +9,11 @@ ASlashCharacterBase::ASlashCharacterBase()
  	
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");//Fname不需要使用TEXT宏
+	//创建一个附着在Mesh上的武器插槽
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	//设置武器不存在碰撞
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 
